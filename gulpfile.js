4120929a00b716dc51e9cc719d017224b04a3c31
asakon <kondoasako@gmail.com>
2018-06-27 18:44:21 +0900

    配布されたgulpfile.jsが動かせないので自前のを使う

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,34 @@
-var gulp          = require('gulp');
-var $             = require('gulp-load-plugins')();
-var browserSync   = require('browser-sync');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var postcss = require('gulp-postcss');
+var cssdeclsort = require('css-declaration-sorter');
 
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([cssdeclsort({order: 'smacss'})]))
+    .pipe(sourcemaps.write())
+    //.pipe(sass().on('error', sass.logError)) // errorが出た方が私は良いので...
+    .pipe(gulp.dest('./css'));
+});
 
-var paths = {
-  "htmlSrc" : "./*.html",
-  "scssSrc" : "./src/scss/**/*.scss",
-  "jsSrc"   : "./src/js/*.js",
-  "jsLib"   : "./src/js/lib/*.js",
-  "imgSrc"  : "./src/images/**",
-  "rootDir" : "./dist/",
-  "imgDir"  : "./dist/images/",
-  "jsDir"  : "./dist/js/"
-}
-
-
-gulp.task('bs', function() {
-  browserSync.init({
+gulp.task('sass:watch', function() {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+gulp.task('browserSync', function() {
+  browserSync({
     server: {
-      baseDir: "./"
-    },
-    notify  : true,
-    xip     : false
+      baseDir: '.'
+    }
   });
-});
-
-gulp.task('scss', function() {
-  return gulp.src(paths.scssSrc)
-    .pipe($.sourcemaps.init())
-      .pipe($.sass()).on('error', $.sass.logError)
-      .pipe($.autoprefixer({
-        browsers: ['last 2 versions']
-      }))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest(paths.rootDir + 'css'))
-    .pipe($.rename({
-      suffix: '.min'
-    }))
-    .pipe($.csso())
-    .pipe(gulp.dest(paths.rootDir + 'css'))
-    .pipe(browserSync.reload({
-      stream: true,
-      once  : true
-    }));
-});
-
-gulp.task('bs-reload', function() {
-   browserSync.reload();
-});
-
-gulp.task('image', function() {
-  return gulp.src(paths.imgSrc)
-    .pipe($.changed(paths.imgDir))
-    .pipe($.imagemin({
-      optimizationLevel: 3
-    }))
-    .pipe(gulp.dest(paths.imgDir));
-});
-
-gulp.task('js', function() {
-  return gulp.src([paths.jsLib, paths.jsSrc])
-    .pipe($.uglify({preserveComments: 'license'}))
-    .pipe($.concat('main.min.js', {newLine: '\n'})
-    )
-    .pipe(gulp.dest(paths.jsDir));
-});
-
-gulp.task('default', ['image', 'js', 'bs', 'scss', 'bs-reload'], function() {
-  $.watch([paths.htmlSrc],function(e) {
-    gulp.start("bs-reload")
-  });
-  $.watch([paths.scssSrc],function(e) {
-    gulp.start("scss")
-  });
-  $.watch([paths.imgSrc],function(e) {
-    gulp.start("image")
-  });
-  $.watch([paths.jsSrc],function(e) {
-    gulp.start("js")
+  
+  gulp.watch([
+    './**/*.html',
+    './**/*.css'
+   ], function() {
+    browserSync.reload();
   });
 });
