0237a50636c1447bf706393d7da65a11f6383490
asakon <kondoasako@gmail.com>
2018-06-27 22:32:32 +0900

    gulp-load-pluginsを使ってsoucemapする

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+//var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 var postcss = require('gulp-postcss');
 var cssdeclsort = require('css-declaration-sorter');
@@ -12,10 +13,10 @@ var paths = {
 
 gulp.task('sass', function() {
   return gulp.src(paths.scssSrc)
-    .pipe(sourcemaps.init())
+    .pipe($.sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(postcss([cssdeclsort({order: 'smacss'})]))
-    .pipe(sourcemaps.write())
+    .pipe($.sourcemaps.write())
     //.pipe(sass().on('error', sass.logError)) // errorが出た方が私は良いので...
     .pipe(gulp.dest(paths.cssDir));
 });
