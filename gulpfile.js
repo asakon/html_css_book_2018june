6dd25b0ff23bfc05d6bc81b63900bd2062fb4c84
asakon <kondoasako@gmail.com>
2018-06-27 22:37:36 +0900

    postcssをgulp-load-plugingsで記述

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-//var sass = require('gulp-sass');
-//var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
-var postcss = require('gulp-postcss');
 var cssdeclsort = require('css-declaration-sorter');
 
 var paths = {
@@ -15,7 +12,7 @@ gulp.task('sass', function() {
   return gulp.src(paths.scssSrc)
     .pipe($.sourcemaps.init())
     .pipe($.sass({outputStyle: 'compressed'}))
-    .pipe(postcss([cssdeclsort({order: 'smacss'})]))
+    .pipe($.postcss([cssdeclsort({order: 'smacss'})]))
     .pipe($.sourcemaps.write())
     //.pipe(sass().on('error', sass.logError)) // errorが出た方が私は良いので...
     .pipe(gulp.dest(paths.cssDir));
