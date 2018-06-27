a8b137e8a31610a05557ae7735d50bbbaf81291a
asakon <kondoasako@gmail.com>
2018-06-27 22:35:03 +0900

    gulp-sassをgulp-load-pluginで記述

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var sass = require('gulp-sass');
+//var sass = require('gulp-sass');
 //var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 var postcss = require('gulp-postcss');
@@ -14,7 +14,7 @@ var paths = {
 gulp.task('sass', function() {
   return gulp.src(paths.scssSrc)
     .pipe($.sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe($.sass({outputStyle: 'compressed'}))
     .pipe(postcss([cssdeclsort({order: 'smacss'})]))
     .pipe($.sourcemaps.write())
     //.pipe(sass().on('error', sass.logError)) // errorが出た方が私は良いので...
