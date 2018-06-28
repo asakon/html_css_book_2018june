813605d7b60ecf9c00850938338a44ac458fa0ed
asakon <kondoasako@gmail.com>
2018-06-28 22:25:30 +0900

    記事概要スタイリング
    entryPanel.min.cssが出力されてしまう

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var browserSync = require('browser-sync');
 var cssdeclsort = require('css-declaration-sorter');
 
 var paths = {
-  "scssSrc" : "./src/scss/main.scss",
+  "scssSrc" : "./src/scss/**/*.scss",
   "cssDir" : "./dist/css"
 }
 
